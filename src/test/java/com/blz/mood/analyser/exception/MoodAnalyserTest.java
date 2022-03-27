7a1729eb44b75806daea152a3857790af7679ef8
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-27 21:14:13 +0530

    Refactored analysemood with no parameter and return according to the test cases

--- src/test/java/com/blz/mood/analyser/exception/MoodAnalyserTest.java
+++ src/test/java/com/blz/mood/analyser/exception/MoodAnalyserTest.java
@@ -11,4 +11,11 @@ public class MoodAnalyserTest {
         String expected = "SAD";
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void givenHappyMood_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy mood");
+        String actual = moodAnalyser.analyseMood();
+        String expected = "HAPPY";
+        Assert.assertEquals(expected, actual);
+    }
 }
